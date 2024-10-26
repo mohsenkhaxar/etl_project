dd6cc6b15f41326656ef728cd89f61dbb4bd786e
mohkha <mohkha@on.sinch.com>
2024-10-26 22:24:21 +0200

    init

--- scripts/etl/data_processing.py
+++ scripts/etl/data_processing.py
@@ -1,5 +1,5 @@
 from pyspark.sql import SparkSession
 
-spark = SparkSession.builder.appName("GIS Processing").getOrCreate()
+spark = SparkSession.builder.appName("ETL Processing").getOrCreate()
 df = spark.read.csv("hdfs://hadoop:9000/data/gis_data.csv")
 # Perform transformations
--- scripts/etl_dag.py
+++ scripts/etl_dag.py
@@ -1,9 +1,9 @@
 from airflow import DAG
 from airflow.operators.python_operator import PythonOperator
 from datetime import datetime
-from gis_pipeline.data_ingest import fetch_data
+from etl.data_ingest import fetch_data
 
-dag = DAG("gis_data_pipeline", start_date=datetime(2024, 1, 1))
+dag = DAG("etl_data_pipeline", start_date=datetime(2024, 1, 1))
 fetch_task = PythonOperator(
     task_id="fetch_data",
     python_callable=fetch_data,
